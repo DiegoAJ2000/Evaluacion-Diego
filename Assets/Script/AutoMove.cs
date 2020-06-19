811f8b6b208e92edf1876164a4d8964b1faa0f22
Diego <diegoaajimenez2000@gmail.com>
2020-06-19 16:42:00 -0400

    Evaluacion

---
+++ Assets/Script/AutoMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoMove : MonoBehaviour
+{
+    public float speed = 0f;
+    private Rigidbody2D rb2d;
+    public float time = 5;
+    private float actualTime = 0;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = new Vector2(speed, 0);
+
+        rb2d.MovePosition((Vector2)transform.position + move * Time.deltaTime);
+
+        actualTime += Time.deltaTime;
+        if(actualTime >= time)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
---
+++ Assets/Script/Movimiento.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    public GameObject nave1;
+    public GameObject nave2;
+    public GameObject meta;
+    public float vel = 0f;
+    private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            move.y = 1; 
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            move.x = -1;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            move.x = 1;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            move.y = -1;
+        }
+
+        rb2d.MovePosition((Vector2)this.transform.position + (move * vel * Time.deltaTime));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject == nave1)
+        {
+            Debug.Log("AUCH!!!");
+        }
+        if (collision.gameObject == nave2)
+        {
+            Debug.Log("AUCH!!!");
+        }
+        if (collision.gameObject == meta)
+        {
+            Debug.Log("Ganaste!!!");
+        }
+    }
+}
